158dd7a58d21534867ca054692d0286df265f71d
flamekaiser007 <arpitmaurya262005@gmail.com>
2026-04-04 02:28:25 +0530

    update CORS for production

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors({
     "http://localhost:5174",
     "http://localhost:5175",
     "http://localhost:3000",
+    "https://cryptico-frontend.vercel.app" 
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: false
