330fadf2f8057492dc0c58b175401a81a677d695
flamekaiser007 <arpitmaurya262005@gmail.com>
2026-04-04 02:23:12 +0530

    fix Auth to auth lowercase

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./routes/Auth");
+const authRoutes = require("./routes/auth");
 const app = express();
 
 // ✅ Allow all localhost ports
