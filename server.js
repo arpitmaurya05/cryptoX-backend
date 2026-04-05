1dbcefbb2ecbfbe4e92be69992ea9b8d9553c117
flamekaiser007 <arpitmaurya262005@gmail.com>
2026-04-05 16:14:58 +0530

    rename Auth.js to auth.js

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ mongoose
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.log("❌ MongoDB error:", err));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
